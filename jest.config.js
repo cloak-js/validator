65185e438edf001e7355630d1e70f801f0c1295c
Gustavo Gentil <gustavorgentil@outlook.com>
2020-08-26 00:08:50 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  restoreMocks: true,
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
