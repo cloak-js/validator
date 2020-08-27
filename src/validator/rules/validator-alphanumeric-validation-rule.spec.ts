c0763a8b3e107fd59d2916d5d6aaa85c2ea908b2
Gustavo Gentil <gustavorgentil@outlook.com>
2020-08-27 19:39:46 -0300

    test: ensure ValidatorAlphanumericValidationRule works calls isAlphanumeric correctly

---
+++ src/validator/rules/validator-alphanumeric-validation-rule.spec.ts
@@ -0,0 +1,54 @@
+import faker from 'faker';
+import validator from 'validator';
+import makeValidationParams, { ValidationParams } from '@/tests/make-validation-params';
+import { ValidatorAlphanumericValidationRule } from '.';
+
+jest.mock('validator');
+
+type SutTypes = {
+  sut: ValidatorAlphanumericValidationRule;
+  params: ValidationParams;
+}
+
+const makeSut = (): SutTypes => ({
+  sut: new ValidatorAlphanumericValidationRule(),
+  params: makeValidationParams(),
+});
+
+describe('Validator alphanumeric validation rule', () => {
+  it('calls isAlphanumeric with the correct values', () => {
+    const { sut, params } = makeSut();
+
+    const isAlphanumericSpy = jest.spyOn(validator, 'isAlphanumeric');
+
+    sut.passes(faker.lorem.word(), params.value);
+
+    expect(isAlphanumericSpy).toHaveBeenCalledWith(params.value);
+  });
+
+  it('passes if isAlphanumeric returns true', () => {
+    const { sut, params } = makeSut();
+
+    jest.spyOn(validator, 'isAlphanumeric').mockReturnValue(true);
+
+    const validation = sut.passes(params.name, params.value);
+
+    expect(validation.passed).toBeTruthy();
+    expect(validation.name).toBe(params.name);
+    expect(validation.messages.length).toBe(0);
+    expect(validation.received).toBe(params.value);
+  });
+
+  it('fails if isAlphanumeric returns false', () => {
+    const { sut, params } = makeSut();
+
+    jest.spyOn(validator, 'isAlphanumeric').mockReturnValue(false);
+
+    const validation = sut.passes(params.name, params.value);
+
+    expect(validation.passed).toBeFalsy();
+    expect(validation.name).toBe(params.name);
+    expect(validation.messages[0]).toBe(`The ${params.name} may only contain letters and numbers.`);
+    expect(validation.received).toBe(params.value);
+  });
+});
